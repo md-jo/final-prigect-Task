bd275ab10be12d4ec08aa01914e904a1a0097d5b
md-jo <moadxjoban@gmail.com>
2023-06-03 14:48:56 +0300

    task

---
+++ progect/ConsoleApplication17/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList tasks = new ArrayList();
+            
+            Task t1 = new Task();
+
+            int key;
+
+            Console.WriteLine("enter the opertion  1 to add   or 2 to delete  or 3 to search  or 4 to print  or 5 to print-Arraylist");
+        key:
+            int op = Convert.ToInt32(Console.ReadLine());
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("enter name of the task to add");
+                    t1.name = Console.ReadLine();
+                    TaskManager.AddTask(t1,tasks);
+                    goto key;
+                    break;
+                case 2:
+                    Console.WriteLine("enter name of the task to delete");
+                    t1.name = Console.ReadLine();
+                    TaskManager.DeleteTask(t1,tasks);
+                    goto key;
+                    break;
+                case 3:
+                    Console.WriteLine("enter name of the task ");
+                    t1.name = Console.ReadLine();
+                    TaskManager.SearchForTask(t1,tasks);
+                    goto key;
+                    break;
+                case 4:
+                    t1.PrintTask();
+                    goto key;
+                    break;
+                case 5:foreach(var i in tasks)
+            Console.WriteLine(i);
+                    break;
+            }
+
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ progect/ConsoleApplication17/TaskManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication17
+{
+    static class TaskManager
+    {
+        
+       public static  void AddTask(Task t1, ArrayList tasks)
+        {
+            tasks.Add(t1);
+      
+        }
+         public static void DeleteTask(Task t1, ArrayList tasks)
+        {
+            tasks.Remove(t1);
+        }
+         public static bool SearchForTask(Task t1, ArrayList tasks)
+        {
+            return tasks.IndexOf(t1) >= 0 ? true : false; 
+            
+        }
+    }
+}
