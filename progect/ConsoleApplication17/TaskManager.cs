a1bfeb0ed78de80eebf26d37fdd2aa8c992f0fdc
md-jo <133594082+md-jo@users.noreply.github.com>
2023-06-03 21:33:03 +0300

    Update TaskManager.cs

--- progect/ConsoleApplication17/TaskManager.cs
+++ progect/ConsoleApplication17/TaskManager.cs
@@ -9,17 +9,17 @@ namespace ConsoleApplication17
 {
     static class TaskManager
     {
-        
-       public static  void AddTask(Task t1, ArrayList tasks)
+         static ArrayList tasks = new ArrayList();
+       public static  void AddTask(Task t1)
         {
             tasks.Add(t1);
       
         }
-         public static void DeleteTask(Task t1, ArrayList tasks)
+         public static void DeleteTask(Task t1)
         {
             tasks.Remove(t1);
         }
-         public static bool SearchForTask(Task t1, ArrayList tasks)
+         public static bool SearchForTask(Task t1)
         {
             return tasks.IndexOf(t1) >= 0 ? true : false; 
             
